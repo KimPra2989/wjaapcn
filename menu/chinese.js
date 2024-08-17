ce2be699620e3fef946489499bb599c155309b8d
kimpra <dustmqaksdltkfrlfdldi@gmail.com>
2024-08-17 19:57:54 +0900

    한식, 일식, 중식 메뉴 추가

--- menu/chinese.js
+++ menu/chinese.js
@@ -1,8 +1,23 @@
 const CHINESE = [
-  { name: '탕수육', weight: 2 },
-  { name: '짜장면', weight: 4 },
-  { name: '깐풍기', weight: 3 },
-  { name: '마파두부', weight: 2 },
+  { name: '마라탕', weight: 95 },
+  { name: '짜장면', weight: 95 },
+  { name: '짬뽕', weight: 94 },
+  { name: '볶음밥', weight: 93 },
+  { name: '탕수육', weight: 92 },
+  { name: '팔보채', weight: 91 },
+  { name: '마파두부', weight: 90 },
+  { name: '유린기', weight: 89 },
+  { name: '소룡포', weight: 87 },
+  { name: '깐풍기', weight: 86 },
+  { name: '동파육', weight: 85 },
+  { name: '만두', weight: 85 },
+  { name: '계란볶음밥', weight: 84 },
+  { name: '해물탕', weight: 83 },
+  { name: '양장피', weight: 82 },
+  { name: '볶음면', weight: 79 },
+  { name: '유산슬', weight: 76 },
+  { name: '깐풍새우', weight: 66 },
+  { name: '잡채밥', weight: 57 },
 ]
 
 export default CHINESE
--- menu/japanese.js
+++ menu/japanese.js
@@ -1,7 +1,20 @@
 const JAPANESE = [
-  { name: '스시', weight: 5 },
-  { name: '라멘', weight: 3 },
-  { name: '튀김', weight: 2 },
+  { name: '스시', weight: 95 },
+  { name: '라멘', weight: 94 },
+  { name: '우동', weight: 93 },
+  { name: '가츠동', weight: 91 },
+  { name: '오야코동', weight: 89 },
+  { name: '텐동', weight: 88 },
+  { name: '규동', weight: 87 },
+  { name: '돈까스', weight: 85 },
+  { name: '샤브샤브', weight: 83 },
+  { name: '카레라이스', weight: 81 },
+  { name: '야끼소바', weight: 80 },
+  { name: '가츠동', weight: 80 },
+  { name: '모밀', weight: 80 },
+  { name: '가라아게', weight: 65 },
+  { name: '오코노미야키', weight: 65 },
+  { name: '우나기동', weight: 61 },
 ]
 
 export default JAPANESE
