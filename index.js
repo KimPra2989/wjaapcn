0f1a697b2de4a14623c88af18178c9fbd24d6324
kimpra <dustmqaksdltkfrlfdldi@gmail.com>
2024-08-17 10:19:28 +0900

    feat: 선택항목에 아무거나 추가

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ async function recommendMenu() {
         { name: chalk.red('한식'), value: 'korean' },
         { name: chalk.green('중식'), value: 'chinese' },
         { name: chalk.blue('일식'), value: 'japanese' },
+        { name: chalk.yellow('아무거나'), value: 'all' },
       ],
     },
   ])
@@ -28,6 +29,8 @@ async function recommendMenu() {
     menu = CHINESE
   } else if (cuisine === 'japanese') {
     menu = JAPANESE
+  } else {
+    menu = [...KOREAN, ...CHINESE, ...JAPANESE]
   }
 
   const randomMenu = menu[Math.floor(Math.random() * menu.length)]
